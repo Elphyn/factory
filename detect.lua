3286e5f6d98dd200445cd91fce955b7d6fdb46b3
Vlad <elphynkarelov@gmail.com>
2025-07-20 23:00:37 +0900

    Main loop testing

---
+++ detect.lua
@@ -0,0 +1,32 @@
+
+function DetectPeripherals()
+    local list = peripheral.getNames()
+
+    local peripherals = {
+        basins = {},
+        storage = {}
+    } 
+
+
+    for _, name in ipairs(list) do
+        if string.match(name, "^create:basin") then
+            peripherals.basins[name] = { state = "idle"}
+        elseif string.match(name, "^extended_drawers:single_drawer") then
+            table.insert(peripherals.storage, name)
+        end
+
+    end
+
+    print("Basins: ")
+    for name, _ in pairs(peripherals.basins) do
+        print(name)
+    end
+
+    print("Storage: ")
+    for _, name in ipairs(peripherals.storage) do
+        print(name)
+    end
+    
+    return peripherals
+end
+
---
+++ main.lua
@@ -0,0 +1,34 @@
+
+
+local peripherals = require("detect")
+
+local queue = {}
+
+local function fmt(str, vars)
+    return (str:gsub("{(.-)}", function(key)
+        return tostring(vars[key] or "{"..key.."}")
+    end))
+end
+
+while true do
+    for _, drawer in ipairs(peripherals.storage) do
+        local info = drawer.items()
+        local count = info.count
+        if count < 256 then
+            if not queue[drawer] then
+                queue[drawer] = {
+                    name = info.name,
+                    need = 256 - count,
+                    state = "queued"
+                }
+            end
+        end
+    end
+    
+    term.clear()
+    for storage_unit, table in pairs(queue) do
+        local line = fmt("Order for {storage_unit} | {table.count} {table.name}")
+    end
+    
+    sleep(5)
+end
