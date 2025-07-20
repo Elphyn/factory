942903693afb799de200344212dfa2c9d56fc4ef
Vlad <elphynkarelov@gmail.com>
2025-07-20 21:48:53 +0900

    testing

--- test.lua
+++ test.lua
@@ -1,7 +1,17 @@
 
 local list = peripheral.getNames()
 
+local basins = {}
 
+i = 0
 for _, device in ipairs(list) do
-    print(device)
+    if strings.match(device, "^create:basin") then
+        basins[i] = device
+        i = i + 1
+    end
+end
+
+for num, basin in ipairs(basin) do
+    line = string.format("%d Basin: %s", num, basin)
+    print(line)
 end
