ccb11cdd93debe79269b1ce5a700cd5de8727ad4
Vlad <elphynkarelov@gmail.com>
2025-07-21 01:07:30 +0900

    Working on processing

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 --
 local detect = require("detect")
 local storage = require("storage")
-
+local recipes = require("recipes")
 
 local peripherals = detect.DetectPeripherals()
 
@@ -12,12 +12,13 @@ local available_items = storage.UpdateStorage(peripherals.storage)
 local queue = {}
 while true do
     -- ordering 
+    term.clear()
     for _, drawer in ipairs(peripherals.storage) do
         local device = peripheral.wrap(drawer)
         local info = device.items()[1]
         local count = info.count
         if count < 256 then
-            if not queue[drawer] then
+            if not queue[drawer] and recipes[info.name] ~= nil then
                 queue[drawer] = {
                     name = info.name,
                     need = 256 - count,
@@ -34,11 +35,8 @@ while true do
         print(string.format("Order for %s: %d %s", name, order.need, order.name))
     end 
     
+    -- processing orders
 
-    
-    
-    
-    
 
     
     sleep(5)
--- recipes.lua
+++ recipes.lua
@@ -1,3 +1,6 @@
 return {
-    
+    ["minecraft:gravel"] = {
+        crafter = "basin",
+        items = {"minecarft:cobblestone"}
+    } 
 }
