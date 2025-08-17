b00b34c68798b2b340bea5de27bdb4bc1d5a409e
Vlad <elphynkarelov@gmail.com>
2025-08-18 00:16:42 +0900

    Added queue, probably a lot of bugs

--- main.lua
+++ main.lua
@@ -5,6 +5,13 @@
 -- Find storage, check if it's not full
 --
 --
+
+local recipes = {
+	["minecraft:gravel"] = {
+		["minecraft:cobblestone"] = 1,
+	},
+}
+
 local function getStorageUnits()
 	local list = peripheral.getNames()
 
@@ -53,9 +60,66 @@ local function findMonitor()
 	return nil
 end
 
-local function displayStorageItems()
-	local itemTable = getStorageItems()
 
+local function initStations(stationStartsWith)
+	local devices = peripherals.getNames()
+
+	local stationTable = {}
+	local stationStack = {}
+
+	for _, name in ipairs(devices) do
+		if string.match(name, "^" .. stationStartsWith) then
+			table.insert(stationStack, name)
+			stationTable[name] = { state = "idle" }
+		end
+	end
+	return stationTable, stationStack
+end
+
+
+local function whatCanCraft(itemsToCraft, itemList)
+	-- probably something like {"minecarft:gravel = {count = 10}"}
+	-- if we got here, assume regent is in recipe
+  --
+  local itemList = itemList
+	local canCraft = {}
+	for name, info in pairs(itemsToCraft) do
+		local maxCraft = info.count
+		for neededIngridientName, needed in pairs(recipes[name]) do
+			local stock = itemList[neededIngridientName] and itemList[neededIngridientName].count or 0
+			local maxByIngridient = math.floor(stock / needed)
+      if maxByIngridient == 0 then
+        goto continue
+      end
+			if maxCraft > maxByIngridient then
+				maxCraft = maxByIngridient
+			end
+		end  
+    local curOrder = {order = name, count = maxCraft}
+		for neededIngridientName, needed in pairs(recipes[name]) do
+      itemList[neededIngridientName].count = itemList[neededIngridientName].count - maxCraft * needed 
+    end
+    canCraft[name] = curOrder
+    ::continue::
+	end
+  return canCraft
+end
+-- should wait until we've crafted a batch, then it can look into crafting somethng else
+local function scheduler(itemTable)
+	local stationTable, stationStack = initStations("create:mill")
+
+	local queue = {}
+	for item, info in pairs(itemTable) do
+		if info.count < info.capacity and recipes[item] ~= nil then
+			-- not enough items + there's a recipe, now we need to check for dependencies
+      queue[item] = {count = item.capacity - info.count}
+		end
+	end
+  queue = whatCanCraft(queue)
+  return queue 
+end
+
+local function displayStorageItems(itemTable)
 	local monitorName = findMonitor()
 	if monitorName == nil then
 		error("No monitor found")
@@ -69,13 +133,40 @@ local function displayStorageItems()
 		monitor.write(itemInfoString)
 		line = line + 1
 	end
+  -- items to craft:
+  local queue = scheduler(itemTable)
+  
+  -- name = {order = name, count = how much we crafting}
+  line = line + 1
+  monitor.write("Queue: ")
+  line = line + 1
+  for name, info in pairs(queue) do
+    local itemInfoString = string.format("%s | Can craft: %d", name, info.count)
+    monitor.write(itemInfoString)
+    line = line + 1
+  end
 end
 
-local function displayLoop()
+local function displayLoop(itemTable)
 	while true do
-		displayStorageItems()
-		sleep(2)
+		displayStorageItems(itemTable)
+		sleep(0.1)
 	end
 end
 
-parallel.waitForAll(displayLoop)
+local function wrapper(func, ...)
+	local args = { ... }
+	return function()
+		return func(table.unpack(args))
+	end
+end
+
+local function main()
+	local itemTable = getStorageItems()
+
+	parallel.waitForAll(wrapper(displayLoop, itemTable))
+end
+
+main()
+
+-- scheduler
---
+++ test.lua
@@ -0,0 +1,9 @@
+local function test(x)
+	local x = x
+	x = x + 1
+	print(x)
+end
+
+a = 2
+test(a)
+print(a)
