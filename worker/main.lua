698bed5535d0de5332e5fb55dce5c9743bebcd38
Vlad <elphynkarelov@gmail.com>
2025-08-20 23:13:54 +0900

    No need to check for type of PC if there's a clean separation

--- worker/main.lua
+++ worker/main.lua
@@ -2,11 +2,6 @@ local getStations = require("stations")
 local config = require("config")
 local handleInstructions = require("dispatcher")
 local function main()
-	if not config.pcType then
-		print("The worker needs a manual set up")
-		return
-	end
-
 	if not rednet.isOpen() then
 		rednet.open(config.modemLocation)
 	end
--- worker/startup.lua
+++ worker/startup.lua
@@ -1,5 +1,5 @@
 if fs.exists("factory/worker/main.lua") then
-	shell.run("main.lua")
+	shell.run("factory/worker/main.lua")
 else
 	print("Something went wrong, there's no main file")
 end
