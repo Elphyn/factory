d32590b5fb555b29ef69174e3c6eeac2f2188846
Vlad <elphynkarelov@gmail.com>
2025-08-26 03:35:46 +0900

    trying to make a smaller env to fish for problem

--- worker/test_main.lua
+++ worker/test_main.lua
@@ -1,5 +1,5 @@
 local getStations = dofile("factory/worker/stations.lua")
-local dispatcher = dofile("factory/worker/dispatcher.lua")
+-- local dispatcher = dofile("factory/worker/dispatcher.lua")
 
 local queue = {
 	{
@@ -14,4 +14,49 @@ local queue = {
 
 local stationStates, stationsAvailable = getStations()
 
-dispatcher(queue[1].task, stationsAvailable, stationStates)
+local function dispatcher(order)
+	-- you get task = {item = "minecraft:gravel", count = 10}
+	-- checking if there are any staions available
+	print("DEBUG: dispatcher started with n = ", #stationsAvailable)
+	while #stationsAvailable < 1 do
+		sleep(0.1)
+	end
+	print("Dispatcher assigning stations: n = " .. #stationsAvailable)
+	local threader = Threader.new()
+	while order.count > 0 or threader:alive() do
+		-- assaigning stations
+		if #stationsAvailable > 0 then
+			local total = #stationsAvailable
+			for i = 1, total do
+				if order.count > 0 then
+					local miniTask = {
+						order = order.item,
+						count = 1,
+					}
+					order.count = order.count - 1
+					local station = popStation(stationsAvailable)
+					stationStates[station].state = "working"
+					threader:addThread(function()
+						print("DEBUG: Starting crafting")
+						craft(buffer, buffer, station, miniTask)
+					end, function(info)
+						print("DEBUG: dispatcher callback")
+						if info.station == nil then
+							print("info.station is nil")
+						end
+						print("Station finished it's piece")
+						stationStates[info.station].state = "idle"
+						table.insert(stationsAvailable, info.station)
+						print("DEBUG: Callback is finished")
+						sleep(0.1)
+					end, { station = station })
+				end
+			end
+		end
+		print("DEBUG: in dispatecher before checking threads")
+		threader:run()
+		sleep(0.1)
+	end
+end
+
+dispatcher(queue[1].task)
