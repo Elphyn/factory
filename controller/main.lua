00c4a62d7f3df3f8d55d5352233e09228c1df17d
Vlad <elphynkarelov@gmail.com>
2025-08-27 17:22:15 +0900

    Trying to make display on controller faster

--- controller/main.lua
+++ controller/main.lua
@@ -1,15 +1,48 @@
+-- local scheduler = require("scheduler")
+--
+-- local function displayLoop()
+-- 	while true do
+-- 		local itemTable = getStorageItems()
+-- 		local queue = scheduler(itemTable)
+-- 		displayStorageItems(itemTable, queue)
+--
+-- 		sleep(0.1)
+-- 	end
+-- end
+--
+-- displayLoop()
 local getStorageItems = require("storage")
+local Threader = dofile("factory/utils/threader.lua")
 local scheduler = require("scheduler")
 local displayStorageItems = require("display")
 
-local function displayLoop()
-	while true do
-		local itemTable = getStorageItems()
-		local queue = scheduler(itemTable)
-		displayStorageItems(itemTable, queue)
+local threader = Threader.new()
 
-		sleep(0.1)
+local itemTable = getStorageItems()
+local queue = scheduler(itemTable)
+
+local function main()
+	threader:addThread(function()
+		-- display items
+		while true do
+			displayStorageItems(itemTable, queue)
+			sleep(0.05)
+		end
+	end)
+
+	threader:addThread(function()
+		-- update storage
+		while true do
+			itemTable = getStorageItems()
+			queue = scheduler(itemTable)
+			sleep(0.05)
+		end
+	end)
+
+	-- main loop
+	while true do
+		threader:run()
 	end
 end
 
-displayLoop()
+main()
