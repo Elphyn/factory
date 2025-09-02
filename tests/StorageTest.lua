90593c441db22a02853e3d5e1f418e4298740788
Vlad <elphynkarelov@gmail.com>
2025-09-03 00:03:23 +0900

    testing pull

--- tests/StorageTest.lua
+++ tests/StorageTest.lua
@@ -9,6 +9,7 @@ local item = "minecraft:cobblestone"
 local function main()
 	storageManager:update()
 	storageManager:pushItem(buffer, item, 10)
+	storageManager:pullItem(buffer, "minecraft:cobblestone", 10)
 end
 
 main()
