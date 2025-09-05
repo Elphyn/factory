f18b1ac0b6678c92929323969f296d1fa2ad0d30
Vlad <elphynkarelov@gmail.com>
2025-09-06 03:25:36 +0900

    Added listening for network on main pc

--- controller/main.lua
+++ controller/main.lua
@@ -16,6 +16,14 @@ local display = Display.new(eventEmitter)
 
 local function main()
 	rednet.open("back")
+
+	threader:addThread(function()
+		-- listening for updates from nodes:
+		while true do
+			networkManager:listen()
+		end
+	end)
+
 	threader:addThread(function()
 		-- updating and displaying contents of storage
 		while true do
