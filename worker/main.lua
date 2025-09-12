7613f40f73d6a57dc4c6373303d7231c3a34ec4e
Vlad <elphynkarelov@gmail.com>
2025-09-12 21:29:12 +0900

    Debugging why new event system doesn't work

--- worker/main.lua
+++ worker/main.lua
@@ -29,6 +29,13 @@ local function main()
 		end
 	end)
 
+	threader:addThread(function()
+		while true do
+			eventEmitter:handleEvents()
+			sleep(0.05)
+		end
+	end)
+
 	while true do
 		threader:run()
 	end
