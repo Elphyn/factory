5a36ea81a2b49e5852a1c7074b6c0b3c383df5e7
vlad <elphynkarelov@gmail.com>
2026-01-21 22:41:22 +0900

    chore: recipes moved in another dir

---
+++ libs/recipes.lua
@@ -0,0 +1,47 @@
+--- @alias ratio number how many of this item need for crafting one of requested
+---
+--- @class recipe
+--- @field dependencies table<itemName, ratio>
+--- @field fluids? table TODO: in future
+---
+--- @class recipes
+--- @field stationTypes table<itemName, stationType>
+--- @field recipes table<itemName, recipe>
+---
+--- @type recipes
+return {
+	stationTypes = {
+		["minecraft:gravel"] = "mill",
+		["minecraft:flint"] = "mill",
+		["minecraft:iron_ingot"] = "press",
+		["minecraft:iron_block"] = "press",
+		["minecraft:iron_nugget"] = "fan:washing",
+	},
+	recipes = {
+		["minecraft:gravel"] = {
+			dependencies = {
+				["minecraft:cobblestone"] = 1,
+			},
+		},
+		["minecraft:flint"] = {
+			dependencies = {
+				["minecraft:gravel"] = 1,
+			},
+		},
+		["minecraft:iron_ingot"] = {
+			dependencies = {
+				["minecraft:iron_nugget"] = 9,
+			},
+		},
+		["minecraft:iron_block"] = {
+			dependencies = {
+				["minecraft:iron_ingot"] = 9,
+			},
+		},
+		["minecraft:iron_nugget"] = {
+			dependencies = {
+				["minecraft:gravel"] = 1 / 0.12,
+			},
+		},
+	},
+}
