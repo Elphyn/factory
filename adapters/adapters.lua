daee6c7a5d0e6ca1ec3d0293a1c2e5ec70d64f1c
vlad <elphynkarelov@gmail.com>
2026-01-22 23:24:00 +0900

    WIP: correct path

--- adapters/adapters.lua
+++ adapters/adapters.lua
@@ -2,7 +2,7 @@
 ---@type modAdapterStorage
 return {
 	["unlimited"] = {
-		["minecraft:chest"] = require("adapters.unlimited.chest"),
+		["minecraft:chest"] = require("adapters.regular.storage.chest"),
 		["extended_drawers:single_drawer"] = require("adapters.unlimited.extended_drawer"),
 		["extended_drawers:double_drawer"] = require("adapters.unlimited.extended_drawer"),
 		["extended_drawers:quad_drawer"] = require("adapters.unlimited.extended_drawer"),
