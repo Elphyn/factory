4bd4491564d1779b68c767c10b0777806df74541
vlad <elphynkarelov@gmail.com>
2026-01-23 16:29:25 +0900

    fix: correct paths

--- adapters/adapters.lua
+++ adapters/adapters.lua
@@ -3,8 +3,8 @@
 return {
 	["unlimited"] = {
 		["minecraft:chest"] = require("adapters.regular.storage.chest"),
-		["extended_drawers:single_drawer"] = require("adapters.unlimited.extended_drawer"),
-		["extended_drawers:double_drawer"] = require("adapters.unlimited.extended_drawer"),
-		["extended_drawers:quad_drawer"] = require("adapters.unlimited.extended_drawer"),
+		["extended_drawers:single_drawer"] = require("adapters.unlimited.storage.extended_drawer"),
+		["extended_drawers:double_drawer"] = require("adapters.unlimited.storage.extended_drawer"),
+		["extended_drawers:quad_drawer"] = require("adapters.unlimited.storage.extended_drawer"),
 	},
 }
